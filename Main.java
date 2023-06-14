050511e658f0a3e1f2717ccd053ce3f2632b254d
rgraba <127319214+rgraba@users.noreply.github.com>
2023-06-14 12:40:50 +0000

    SkończoneZadanie

--- Main.java
+++ Main.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    System.out.println("Program sumujący elementy tablicy jednowymiarowej");
+     Scanner scanner = new Scanner(System.in);
+    while(true){
+    System.out.println("Podaj liczbę elementów tablicy: ");
+        int LiczebnośćTablicy = scanner.nextInt();
+        int suma = 0;
+        int[] liczby = new int[LiczebnośćTablicy];
+
+        System.out.println("Podaj elementy tablicy:");
+        for (int i = 0; i < LiczebnośćTablicy; i++) {
+          System.out.print("Element " + (i + 1) + ": ");
+            liczby[i] = scanner.nextInt();
+            suma = suma + liczby[i];
+        }
+        System.out.println("Suma elementów tablicy to: " + suma); 
   }
+}
 }
